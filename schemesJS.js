c9cad9c0155c525fb6684d03a48429c259b9a279
avishsa <avishag1991@gmail.com>
2021-01-10 21:33:44 +0200

    basic security policy

--- schemesJS.js
+++ schemesJS.js
@@ -1,13 +1,35 @@
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
 
-const Joi = require('joi');
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundJSScheme = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
         // image :Joi.string().required(),
-        location: Joi.string().required(),
-        description :Joi.string().required(),
+        location: Joi.string().required().escapeHTML(),
+        description :Joi.string().required().escapeHTML(),
 
     }).required(),
     deleteImages:Joi.array()
@@ -15,7 +37,7 @@ module.exports.campgroundJSScheme = Joi.object({
 
 module.exports.reviewJSScheme = Joi.object({
     review : Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().required().min(0).max(5)
     })
 })
